158d88fb790b6c8393ef3d22ad70ab077bf17218
jyll sarta <illstarred.dive@gmail.com>
2019-11-09 12:36:42 +0900

    twitterへPOSTする機能を追加

--- airryr_web.rb
+++ airryr_web.rb
@@ -1,11 +1,14 @@
-require 'sinatra'
-require 'dotenv/load'
+require "sinatra"
+require "dotenv/load"
+require_relative "twitterAPI"
 
-get '/' do
-  'Hello world!' 
+get "/" do
+  "Hello world!" 
 end
 
 post "/#{ENV["WEBHOOK_ENDPOINT"]}" do
   params = JSON.parse(request.body.read, symbolize_names: true)
-  "ばかばかっ！値は#{params[:alert][:metricValue]}でステータスは#{params[:alert][:status]}よ！ちゃんと換気しなさい！"
+  msg = "ばかばかっ！値は#{params[:alert][:metricValue]}でステータスは#{params[:alert][:status]}よ！ちゃんと換気しなさい！"
+  TwitterAPI.post(msg) if ENV["POST_TO_TWITTER"] == "y"
+  msg
 end
---
+++ twitterAPI.rb
@@ -0,0 +1,14 @@
+require "dotenv/load"
+require "twitter"
+
+class TwitterAPI
+  def self.post(messsage)
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_KEY"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      end
+    client.update(messsage)
+  end
+end
