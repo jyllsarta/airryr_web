e3b13daf320e7389284c93a5c16cbc4a4af6413f
jyll sarta <illstarred.dive@gmail.com>
2019-11-16 16:44:06 +0900

    openが古いアラートは無視させる

--- airryr_web.rb
+++ airryr_web.rb
@@ -2,6 +2,10 @@ require "sinatra"
 require "dotenv/load"
 require_relative "twitterAPI"
 
+def stale_alert?(alert_opened_at)
+  Time.at(alert_opened_at) + ENV["IGNORE_ALERT_OPENED_AT_SECONDS"].to_i < Time.now
+end
+
 get "/" do
   "Hello world!" 
 end
@@ -17,6 +21,10 @@ post "/#{ENV["WEBHOOK_ENDPOINT"]}" do
   when "critical"
     msg = "@jyll ばかばかっ！あんたの部屋の二酸化炭素濃度が#{params[:alert][:metricValue]}ppmまで上がってるわよ！ ステータスでいうと#{params[:alert][:status]}なんだから！ちゃんと換気しなさい！"
   end
+
+  # warn -> crit -> warn という時系列で発報された場合の2度目のwarnは無視させたいため、
+  # IGNORE_ALERT_OPENED_AT_SECONDS 以上古いWarnのアラートは無視する
+  return if stale_alert?(params[:alert][:openedAt]) && params[:alert][:status] == "warning"
   TwitterAPI.post(msg) if ENV["POST_TO_TWITTER"] == "y"
   msg
 end
