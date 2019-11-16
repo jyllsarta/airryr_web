706999d00ed2c38bb02ff0de88f6d17e387c0b13
jyll sarta <illstarred.dive@gmail.com>
2019-11-16 17:16:17 +0900

    メッセージをymlに外だし

--- airryr_web.rb
+++ airryr_web.rb
@@ -1,6 +1,9 @@
 require "sinatra"
 require "dotenv/load"
 require_relative "twitterAPI"
+require "yaml"
+
+messages = YAML.load_file("messages.yml")
 
 def stale_alert?(alert_opened_at)
   Time.at(alert_opened_at) + ENV["IGNORE_ALERT_OPENED_AT_SECONDS"].to_i < Time.now
@@ -12,19 +15,12 @@ end
 
 post "/#{ENV["WEBHOOK_ENDPOINT"]}" do
   params = JSON.parse(request.body.read, symbolize_names: true)
-  # TODO: 文言にランダム性とかアラート用画像とか用意したいね
-  case params[:alert][:status]
-  when "ok"
-    msg = "@jyll 換気終わったみたいね！えらいわ！"
-  when "warning"
-    msg = "@jyll 二酸化炭素濃度が#{params[:alert][:metricValue]}ppmまで上がってるから、そろそろ換気しなさいっ！ ステータスでいうと#{params[:alert][:status]}くらいよ！"
-  when "critical"
-    msg = "@jyll ばかばかっ！あんたの部屋の二酸化炭素濃度が#{params[:alert][:metricValue]}ppmまで上がってるわよ！ ステータスでいうと#{params[:alert][:status]}なんだから！ちゃんと換気しなさい！"
-  end
+  status = params[:alert][:status]
+  msg = messages[status].sample % params[:alert][:metricValue]
 
   # warn -> crit -> warn という時系列で発報された場合の2度目のwarnは無視させたいため、
   # IGNORE_ALERT_OPENED_AT_SECONDS 以上古いWarnのアラートは無視する
-  return if stale_alert?(params[:alert][:openedAt]) && params[:alert][:status] == "warning"
+  return if stale_alert?(params[:alert][:openedAt]) && status == "warning"
   TwitterAPI.post(msg) if ENV["POST_TO_TWITTER"] == "y"
   msg
 end
