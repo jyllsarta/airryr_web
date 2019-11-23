816e1d9bda9e59864bddaa253323cae312de1f99
jyll sarta <illstarred.dive@gmail.com>
2019-11-23 19:12:44 +0900

    デプロイスクリプトを修正

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git pull
 echo "bundle install"
 bundle install
 echo "kill older server"
-PID=`ps ax | grep [a]irryr_web | awk '{ print $1 }'`
+PID=`ps ax | grep -e '[a]irryr_web' | awk '{ print $1 }'`
 kill ${PID}
 echo "start server"
-bundle exec ruby airryr_web.rb -e production >> airryr_web.log  2>&1 &
+APP_ENV=production bundle exec ruby airryr_web.rb >> airryr_web.log  2>&1 &
 echo "deploy done."
